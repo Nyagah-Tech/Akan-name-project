3e6b85d81088964bdee32fe3cdb9fd7cdbd98e8e
dan <dan@localhost.localdomain>
2019-09-20 21:16:49 +0300

    code javascript code to take the users input

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var male=[
+    "Kwasi","Kwadwo","kwabena","kwaku","yaw","kofi","kwame"
+]
+var female=[
+    "Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"
+]
+
+function calculator(){
+    
+    var name=""
+    var gender= document.forms["myForm"]["gender"].value;
+    var date=document.forms["myForm"]["date"].value;
+    var dateObject=new Date(date);
+    var day=dateObject.getDay();
+    if (gender==="male"){
+        name=male[day];
+    }else if (gender==="female"){
+        name=female[day];
+    }else{
+        console.log("invalid option")
+    }
+    document.getElementById("result").innerHTML=name;
+    return name;
+  
+}
